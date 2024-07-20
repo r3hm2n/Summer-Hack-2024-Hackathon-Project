9aa7fc52d3e140d021950d5814351733cad9d249
Abdul Rehman <manohafiz08@gmail.com>
2024-07-20 16:16:56 -0700

    updated work

---
+++ work/script.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  setTimeout(() => {
+    loader.classList.add("loader-hidden");
+  }, 3000);
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
+
+
+let $ = (e) => document.querySelector(e);
+
+
+let dots = $(".dots");
+
+function animate(element, className) {
+  element.classList.add(className);
+  setTimeout(() => {
+    element.classList.remove(className);
+    setTimeout(() => {
+      animate(element, className);
+    }, 500);
+  }, 2500);
+}
+
+animate(dots, "dots--animate");
+
+
+
+// -----------------On-click animation---------------------------
+document.querySelector("button").addEventListener('mouseenter', function() {
+   document.querySelector('.moon').style.transform = "scale(150%)"
+
+  });
